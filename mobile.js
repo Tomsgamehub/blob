4b53b738837d06f9952d6d27aadf02e18d3b814e
Tomsgamehub <alex.lewinen@googlemail.com>
2026-06-24 13:25:16 +0200

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -192,11 +192,11 @@ function getKeys() {
 			let i = 0;
 			do {
 				i+= 1;
-				c[i] = R[i];
-				//c[i] = R.C[i];
+				c[i] = R.C[i];
 			} while (i < 30);
 		update();
 		}
+		console.log("keys are gotten. C is: ", R);
 }
 
 
@@ -209,6 +209,7 @@ function setKey() {
   localStorage.setItem("lastPlate", lastPlate);
   localStorage.setItem("lastColor", lastColor);
   localStorage.setItem("points", points);
+	console.log("key is set for C: ", localStorage.getItem("C"));
 //	chrome.storage.local.set({ C: ["null", c[1], c[2], c[3], c[4], c[5], c[6], c[7], c[8], c[9], c[10], c[11], c[12], c[13], c[14], c[15], c[16], c[17], c[18], c[19], c[20], c[21], c[22], c[23], c[24], c[25], c[26], c[27], c[28], c[29], c[30] ] });
 //     	chrome.storage.local.set({ lastPlate: lastPlate });
 //     	chrome.storage.local.set({ lastColor: lastColor });
